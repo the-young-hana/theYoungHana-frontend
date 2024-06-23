44171cb23ff9724135d96962ac16a96331acec1b
EunSo0 <dmstn423@naver.com>
2024-06-23 16:33:08 +0900

    ♻️ 라우팅 outlet 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import { StudentCard } from "./pages/studentCard/StudentCard";
-import { Story } from "./pages/story/Story";
-import { Reward } from "./pages/reward/Reward";
-import { Knowledge } from "./pages/knowledge/Knowledge";
-import { Event } from "./pages/event/Event";
-import Main from "./pages/Main";
-import IPhoneFrame from "./components/common/IPhoneFrame";
 
 function App() {
   return (
-    <IPhoneFrame>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/studentCard" element={<StudentCard />} />
-        <Route path="/event" element={<Event />} />
-        <Route path="/story" element={<Story />} />
-        <Route path="/reward" element={<Reward />} />
-        <Route path="/knowledge" element={<Knowledge />} />
-      </Routes>
-    </IPhoneFrame>
+    <div>
+      <Outlet />
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { StudentCard } from "./pages/studentCard/StudentCard";
+import { Story } from "./pages/story/Story";
+import { Reward } from "./pages/reward/Reward";
+import { Knowledge } from "./pages/knowledge/Knowledge";
+import { Event } from "./pages/event/Event";
 import IPhoneFrame from "./components/common/IPhoneFrame.tsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        element: <IPhoneFrame />,
+        children: [
+          { path: "", element: <Home /> },
+          { path: "studentCard", element: <StudentCard /> },
+          { path: "event", element: <Event /> },
+          { path: "story", element: <Story /> },
+          { path: "reward", element: <Reward /> },
+          { path: "knowledge", element: <Knowledge /> },
+        ],
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 );
