af07b5528728b89c34479f2c28fc809722d13c2f
hejin8307 <hejin8307@naver.com>
2024-06-24 11:06:14 +0900

    🔀 충돌 해결

--- src/pages/story/Story.tsx
+++ src/pages/story/Story.tsx
@@ -1,8 +1,10 @@
 import { NavigationBar } from "../../components/common/NavigationBar";
+import { TopBar } from "../../components/common/TopBar";
 
 export const Story = () => {
   return (
     <div>
+      <TopBar title="스토리" />
       Story
       <NavigationBar />
     </div>
