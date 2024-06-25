6a964bba91f2f49cbcb0bc42814a0ce75f750e22
Eunsoo <101039358+EunSo0@users.noreply.github.com>
2024-06-25 14:08:30 +0900

    ♻️ MenuBar 공통컴포넌트로 분리 (#14)

---
+++ src/components/common/MenuBar.tsx
@@ -0,0 +1,40 @@
+import { Dispatch, FC, SetStateAction } from "react";
+import cn from "../../utils/cn";
+
+interface IProps {
+  menu1: string;
+  menu2: string;
+  one: boolean;
+  setOne: Dispatch<SetStateAction<boolean>>;
+}
+
+export const MenuBar: FC<IProps> = ({ menu1, menu2, one, setOne }) => {
+  const handleEnd = (type: number) => {
+    type === 1 ? setOne(true) : setOne(false);
+  };
+
+  return (
+    <div className="flex w-full justify-around py-2 bg-hanaBgGray">
+      <div
+        className={cn(
+          one ? "font-bold border-b-[3px] text-hanaGreen border-hanaGreen" : "",
+          "cursor-pointer",
+        )}
+        onClick={() => handleEnd(1)}
+      >
+        {menu1}
+      </div>
+      <div
+        className={cn(
+          !one
+            ? "font-bold border-b-[3px] text-hanaGreen border-hanaGreen"
+            : "",
+          "cursor-pointer",
+        )}
+        onClick={() => handleEnd(2)}
+      >
+        {menu2}
+      </div>
+    </div>
+  );
+};
