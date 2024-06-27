c7b8d1d3026f60169fc568348b2e9f8108ca06ca
EunSo0 <dmstn423@naver.com>
2024-06-27 10:27:29 +0900

    🐛 반응형 버그 수정2

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -28,7 +28,7 @@ export const Login = () => {
       <div className="flex flex-col justify-between h-full bg-dark">
         <StatusBar className="absolute top-0 z-20 !bg-dark font-medium" white />
         <TopBar white bgdark />
-        <div className="flex flex-col items-center w-full">
+        <div className="flex flex-col items-center w-full pt-4">
           <span className="mb-8 text-xl text-white"> 간편비밀번호 입력 </span>
           <div className="flex flex-row gap-4 mb-8">
             {[0, 1, 2, 3, 4, 5].map((index) => (
