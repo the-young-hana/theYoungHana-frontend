2207de60571fb7f2c7692708d7d21b754ef86f15
EunSo0 <dmstn423@naver.com>
2024-07-02 15:05:03 +0900

    ✨ 기타 변경사항 수정

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -3,16 +3,18 @@ import cn from "../../utils/cn";
 interface LoadingProps {
   show: boolean;
   label?: string;
+  back?: boolean;
 }
 
-export const Loading = ({ show, label = "" }: LoadingProps) => {
+export const Loading = ({ show, label = "", back = true }: LoadingProps) => {
   return (
     <div
       className={cn(
-        "flex absolute top-0 left-0 w-iPhone h-iPhone z-30 rounded-3xl transition-opacity justify-center items-center  backdrop-blur-sm bg-black/70",
+        "flex absolute top-0 left-0 w-iPhone h-iPhone z-30 rounded-3xl transition-opacity justify-center items-center",
         show
           ? "pointer-events-auto"
           : "pointer-events-none opacity-0 delay-500",
+        back ? " backdrop-blur-sm bg-black/70" : "",
       )}
     >
       <div className="flex flex-row">
@@ -20,7 +22,7 @@ export const Loading = ({ show, label = "" }: LoadingProps) => {
         <div className="w-3 h-3 rounded-full bg-hanaGreen animate-[bounce_1s_infinite_100ms]" />
         <div className="w-3 h-3 rounded-full bg-hanaGreen animate-[bounce_1s_infinite_200ms]" />
       </div>
-      <span className="text-white">{label}</span>
+      <span className={cn(back ? "text-white" : "text-black")}>{label}</span>
     </div>
   );
 };
