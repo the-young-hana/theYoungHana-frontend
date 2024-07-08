844c6c76888664c01bc3707208f7524efd32e8e4
EunSo0 <dmstn423@naver.com>
2024-07-08 17:51:07 +0900

    🐛 학생증 qr 새로고침 수정

--- src/types/studentCard.d.ts
+++ src/types/studentCard.d.ts
@@ -1,11 +1,14 @@
 interface StudentCardType {
   studentIdx: number;
+  memberIdx: number;
   studentName: string;
   studentId: string;
   studentCollege: string;
   studentCollegeImage: string;
   studentDept: string;
+  studentDeptIdx: number;
   studentCardFrontImage: string;
   studentCardBackImage: string;
-  isVertical: boolean;
+  isVertical: true;
+  isAdmin: true;
 }
