93e57a605d9a109d623e5ea04614709ebfe4633a
Hyejin <31836035+hejin8307@users.noreply.github.com>
2024-07-09 13:11:54 +0900

    🐛 Firebase 배포 환경 테스트

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,35 +1,3 @@
-<<<<<<< Updated upstream
-importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js");
-importScripts(
-  "https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js",
-);
-
-firebase.initializeApp({
-  apiKey: "AIzaSyA3X6Hj6zLwPwfUpJAaw2cCEZTp8_sQUfY",
-  authDomain: "theyounghana-ff6c3.firebaseapp.com",
-  projectId: "theyounghana-ff6c3",
-  storageBucket: "theyounghana-ff6c3.appspot.com",
-  messagingSenderId: "466410738142",
-  appId: "1:466410738142:web:0a2fba4c795e9ad2ba0d4f",
-  measurementId: "G-YV2JX2P1JD",
-});
-
-const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage((payload) => {
-  console.log(
-    "[firebase-messaging-sw.js] Received background message ",
-    payload,
-  );
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: "/images/favicon.png",
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
-=======
 importScripts(
   "https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js",
 );
@@ -62,4 +30,3 @@ messaging.onBackgroundMessage((payload) => {
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
->>>>>>> Stashed changes
